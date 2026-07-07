2c4728f59ef7aac622294e1104e3179a25e3d5a0
EduardoLopes17 <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-07-07 07:43:09 -0300

    add - Terminei o exercicio 3

--- ex_03.php
+++ ex_03.php
@@ -1,5 +1,17 @@
 <?php
 
-function mascararCpf($cpf){
-    
+function mascaraCPF($cpf) {
+
+    $mascara = str_repeat("*", strlen($cpf) - 4);
+    $final = substr($cpf, - 4);
+
+    return $mascara . $final;
+
 }
+
+    $cpf = "13889190950";
+
+    echo "$cpf <br>";
+    echo mascaraCPF($cpf);
+
+?>
