d7a70f0578b2def284bd93e644cde097dd35d8b6
EduardoLopes17 <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-07-09 07:56:38 -0300

    add - Terminei o exercicio 6

---
+++ ex_06.php
@@ -0,0 +1,22 @@
+<?php
+
+    function calcularTemperatura($temperatura, $unidadeOrigem, $unidadeDestino) {
+    if ($unidadeOrigem === 'C' && $unidadeDestino === 'F') {
+        return ($temperatura * 9/5) + 32;
+    } 
+    
+    if ($unidadeOrigem === 'F' && $unidadeDestino === 'C') {
+        return ($temperatura - 32) * 5/9;
+    }  
+
+    return $temperatura;
+}
+
+$temperatura = 25;
+$unidadeOrigem = 'C';
+$unidadeDestino = 'F';
+$resultado = calcularTemperatura($temperatura, $unidadeOrigem, $unidadeDestino);
+echo "Temperatura original: " . $temperatura . "°" . $unidadeOrigem . "<br>";
+echo "Temperatura convertida: " . $resultado . "°" . $unidadeDestino;
+
+?>
