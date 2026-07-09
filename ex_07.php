da9df17cad870dc112525b7229f07113df13da69
EduardoLopes17 <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-07-09 09:01:24 -0300

    add - Terminei o exercicio 7

--- ex_07.php
+++ ex_07.php
@@ -1,3 +1,31 @@
 <?php
 
+    function calcularDesconto($Valor){
+
+        if ($Valor > 1000){
+            $Desconto = 30;
+        }elseif ($Valor > 500){
+            $Desconto = 20;
+        }elseif ($Valor > 100){
+            $Desconto = 10;
+        } else {
+            $Desconto = 0;
+        }
+
+        $ValorDesconto =  $Valor * ($Desconto/ 100);
+        $valorFinal  = $Valor - $ValorDesconto;
+
+        return [
+            "ValorOriginal" => $Valor,
+            "Desconto" => $Desconto,
+            "ValorFinal" => $valorFinal
+        ];
+    }
+        $ValorCompra = 777;
+
+        $Resultado = calcularDesconto($ValorCompra);
+
+        echo "Valor da compra: R$ " . number_format($Resultado["ValorOriginal"], 2, ",", ".") . "<br>";
+        echo "Desconto aplicado: " . $Resultado["Desconto"] . "%<br>";
+        echo "Valor final: R$ " . number_format($Resultado["ValorFinal"], 2, ",", ".");
 ?>
